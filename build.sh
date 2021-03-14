b75275cab8217c16e71a045bbc5ec06c04649e36
Hetsh <git@hetsh.de>
2021-03-14 15:41:29 +0100

    Generate required host and client keypairs

--- build.sh
+++ build.sh
@@ -25,13 +25,35 @@ IMG_NAME="hetsh/$APP_NAME"
 docker build --tag "$IMG_NAME:latest" --tag "$IMG_NAME:$_NEXT_VERSION" .
 
 case "${1-}" in
-	# Test with default configuration
+	# Test with temporary host keys
 	"--test")
+		# Create temporary directory
+		TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
+		add_cleanup "rm -rf $TMP_DIR"
+
+		# Host keys
+		assert_dependency "ssh-keygen"
+		ssh-keygen -q -t rsa -N "" -f "$TMP_DIR/ssh_host_rsa_key"
+		ssh-keygen -q -t ecdsa -N "" -f "$TMP_DIR/ssh_host_ecdsa_key"
+		ssh-keygen -q -t ed25519 -N "" -f "$TMP_DIR/ssh_host_ed25519_key"
+
+		# Client keys
+		CLIENT_KEY="$TMP_DIR/ssh_rsync_rsa_key"
+		ssh-keygen -q -t rsa -N "" -f "$CLIENT_KEY"
+		ln -s "$(basename $CLIENT_KEY).pub" "$TMP_DIR/authorized_keys"
+
+		# Apply permissions
+		extract_var APP_UID "./Dockerfile" "\d+"
+		chown -R "$APP_UID" "$TMP_DIR"
+		chmod 755 "$TMP_DIR"
+		chmod 644 "$CLIENT_KEY"
+
 		docker run \
 		--rm \
 		--tty \
 		--interactive \
 		--publish 22:22/tcp \
+		--mount type=bind,source="$TMP_DIR",target=/rsync \
 		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
 		--name "$APP_NAME" \
 		"$IMG_NAME"
